3d0ca17644e40940e4d3b544731d913b6c632a3d
edi-sipka@gmail.com <edisipka@gmail.com>
2022-11-11 11:51:12 +0100

    Add lives to game

--- play.rb
+++ play.rb
@@ -1,7 +1,13 @@
 class Hangman
-  def initalize
+  def initialize
     @letters = ("a".."z").to_a
     @word = words.sample
+    @lives = 7
+    @correct = []
+    @word_length = ""
+    @word.first.size.times do
+      word_length += " _ "
+      end
   end
 
   def words
@@ -14,11 +20,37 @@ class Hangman
   ]
   end
 
-  def begin
-    #start game
-    puts "Welcome to the game... Your clue is #{@word.first}"
+  def tester(last_guess)
+    word_length = ""
+    puts word_length
+  end
+
+  def guess_word 
+   if @lives > 0
+    puts "Enter a letter"
     guess = gets.chomp 
-    puts "You guessed #{guess}"
+    game_guess = @word.first.include? guess
+    if game_guess 
+      puts "You find a letter"
+      @correct.push(guess)
+      @letters.delete guess
+      tester guess
+    else 
+      @lives -= 1
+      puts "You missed it. You have #{ @lives } guesses left"
+      guess_word
+    end
+  else 
+    puts "Game over! Start again."
+  end
+end
+
+  def begin
+    puts "Welcome to the game... Your clue is #{@word.first.size} characters long"
+    tester
+
+    puts "Clue : "" #{ @word.last }"
+   guess_word
 end
 
 end 
